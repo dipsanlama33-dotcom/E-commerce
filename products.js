17f5abed0f0dbe1356ff37d38d6a67eee74cbe58
sushant-bit <sushantadhikari944@gmail.com>
2026-09-09 23:33:01 +0545

    change all the code of the file

--- products.js
+++ products.js
@@ -1,5 +1,5 @@
 const products = [
-  { id: 1, name: "Wireless Headphones", price: 49.99, img: "https://picsum.photos/seed/headphones/300/200" },
+  { id: 1, name: "Wireless Headphones", price: 49.99, img: "https://png.pngtree.com/png-clipart/20250415/original/pngtree-wireless-headphone-png-image_20721964.png" },
   { id: 2, name: "Smart Watch", price: 89.99, img: "https://picsum.photos/seed/smartwatch/300/200" },
   { id: 3, name: "Gaming Mouse", price: 29.99, img: "https://picsum.photos/seed/mouse/300/200" }
 ];
