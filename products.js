c3d1d0de458974ac6e0a2126284bc78424879fb7
dipsanlama33-dotcom <dipsanlama33@gmail.com>
2026-09-09 23:37:04 +0545

    Added images in the product.js

--- products.js
+++ products.js
@@ -1,7 +1,7 @@
 const products = [
-  { id: 1, name: "Wireless Headphones", price: 49.99, img: "https://picsum.photos/seed/headphones/300/200" },
-  { id: 2, name: "Smart Watch", price: 89.99, img: "https://picsum.photos/seed/smartwatch/300/200" },
-  { id: 3, name: "Gaming Mouse", price: 29.99, img: "https://picsum.photos/seed/mouse/300/200" }
+  { id: 1, name: "Wireless Headphones", price: 49.99, img: "https://m.media-amazon.com/images/I/81NfyaGuSVL.jpg" },
+  { id: 2, name: "Smart Watch", price: 89.99, img: "https://www.skg.com/cdn/shop/products/skg-v7-pro-smart-watch-with-alexa-built-in-bluetooth-callanswermake-call-604781.jpg?v=1677051256" },
+  { id: 3, name: "Gaming Mouse", price: 29.99, img: "https://m.media-amazon.com/images/I/71QR4yJdKaL.jpg"}
 ];
 
 const container = document.getElementById("product-container");
