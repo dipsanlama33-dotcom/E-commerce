c01343f37b4b1118b403fad084900d74b84c53fd
sushant-bit <sushantadhikari944@gmail.com>
2026-09-10 10:15:54 +0545

    Added duo

--- products.js
+++ products.js
@@ -1,12 +1,33 @@
 const products = [
-  { id: 1, name: "Wireless Headphones", price: 49.99, img: "https://m.media-amazon.com/images/I/81NfyaGuSVL.jpg" },
-  { id: 2, name: "Smart Watch", price: 89.99, img: "https://www.skg.com/cdn/shop/products/skg-v7-pro-smart-watch-with-alexa-built-in-bluetooth-callanswermake-call-604781.jpg?v=1677051256" },
-  { id: 3, name: "Gaming Mouse", price: 29.99, img: "https://m.media-amazon.com/images/I/71QR4yJdKaL.jpg"}
+  {
+    id: 1,
+    name: "Wireless Headphones",
+    price: 49.99,
+    img: "https://m.media-amazon.com/images/I/81NfyaGuSVL.jpg",
+  },
+  {
+    id: 2,
+    name: "Smart Watch",
+    price: 89.99,
+    img: "https://www.skg.com/cdn/shop/products/skg-v7-pro-smart-watch-with-alexa-built-in-bluetooth-callanswermake-call-604781.jpg?v=1677051256",
+  },
+  {
+    id: 3,
+    name: "Gaming Mouse",
+    price: 29.99,
+    img: "https://m.media-amazon.com/images/I/71QR4yJdKaL.jpg",
+  },
+  {
+    id: 4,
+    name: "iPhone Duo",
+    price: 1999.0,
+    img: "https://media.gq.com/photos/6aa1c3d39a1b576487b2da8c/3:4/w_748%2Cc_limit/Apple-Duo.png",
+  },
 ];
 
 const container = document.getElementById("product-container");
 
-products.forEach(product => {
+products.forEach((product) => {
   const card = document.createElement("div");
   card.className = "product-card";
   card.innerHTML = `
